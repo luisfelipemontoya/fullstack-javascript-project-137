9926446a900707af833ccbff9c80601bbf1ed7cd
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-06-29 01:48:24 -0500

    add basic application layout

--- src/main.js
+++ src/main.js
@@ -6,25 +6,43 @@ app.innerHTML = `
   <div class="container mt-5">
     <div class="row justify-content-center">
       <div class="col-lg-8">
-        <h1 class="mb-4">RSS Reader</h1>
+
+        <h1>RSS Reader</h1>
 
         <form>
+
+          <label for="rss-url">
+            RSS link
+          </label>
+
           <div class="input-group">
+
             <input
+              id="rss-url"
+              name="url"
+              aria-label="url"
               type="text"
               class="form-control"
               placeholder="https://example.com/feed.xml"
             >
+
             <button
               type="submit"
               class="btn btn-primary"
             >
-              Agregar
+              Añadir
             </button>
+
           </div>
+
+          <p
+            id="feedback"
+            class="feedback"
+          ></p>
+
         </form>
+
       </div>
     </div>
   </div>
 `;
-
